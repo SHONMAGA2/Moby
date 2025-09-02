7f4f043b9205b85356b628547b231580731523de
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-09-03 01:05:50 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ return(
 <html lang="en">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta CharSet = "UTF-8" />
+<meta charSet = "UTF-8" />
 <title>Deep Dive Into Moby-Dick</title>
 </head>
 <body>
